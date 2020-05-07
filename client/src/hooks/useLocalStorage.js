2a25da1a33c5f9ef545a013d6c28b831f059227d
Sandra Coburn <sandra_coburn@hotmail.com>
2020-05-07 09:11:42 -0700

    Fixed local storage

--- client/src/hooks/useLocalStorage.js
+++ client/src/hooks/useLocalStorage.js
@@ -2,16 +2,12 @@ import { useState } from "react";
 
 export const useLocalStorage = (key, initialValue) => {
   const [storedValue, setStoredValue] = useState(() => {
-    if (localStorage.getItem(key)) {
-      return JSON.parse(localStorage.getItem(key));
-    } else {
-      localStorage.setItem(key, JSON.stringify(initialValue));
-      return initialValue;
-    }
+    const item = window.localStorage.getItem(key);
+    return item ? JSON.parse(item) : initialValue;
   });
-  const setValue = (newValue) => {
-    setStoredValue(newValue);
-    localStorage.setItem(key, JSON.stringify(newValue));
+  const setValue = (value) => {
+    setStoredValue(value);
+    window.localStorage.setItem(key, JSON.stringify(value));
   };
   return [storedValue, setValue];
 };
