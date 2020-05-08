74edead34e2c606f17cd1d995e0a26f903f066b6
Sandra Coburn <sandra_coburn@hotmail.com>
2020-05-08 14:15:17 -0700

    Added search input, stretch goal

--- client/src/components/PlantList.js
+++ client/src/components/PlantList.js
@@ -7,6 +7,7 @@ export default class PlantList extends Component {
     super();
     this.state = {
       plants: [],
+      query: "",
     };
   }
   // when the component mounts:
@@ -21,35 +22,69 @@ export default class PlantList extends Component {
       })
       .catch((err) => console.log("this error", err));
   }
+  //Stretch goal to add a search form
+  handleChanges = (e) => {
+    this.setState({ query: e.target.value });
+    this.filterPlants(e.target.value);
+  };
+  filterPlants = (query) => {
+    const results = this.state.plants.filter((plant) =>
+      plant.name
+        .toLowerCase()
+        .includes(
+          query.toLowerCase() ||
+            plant.scientificName
+              .toLowerCase()
+              .includes(this.query.toLowerCase()) ||
+            plant.description.toLowerCase().includes(this.query.toLowerCase())
+        )
+    );
+    this.setState({ plants: results });
+  };
 
   /*********  DON'T CHANGE ANYTHING IN THE RENDER FUNCTION *********/
   render() {
-    // console.log("this is plants", plants);
     return (
-      <main className="plant-list">
-        {this.state?.plants &&
-          this.state.plants.map((plant, index) => (
-            <div className="plant-card" key={index}>
-              <img className="plant-image" src={plant.img} alt={plant.name} />
-              <div className="plant-details">
-                <h2 className="plant-name">{plant.name}</h2>
-                <p className="plant-scientific-name">{plant.scientificName}</p>
-                <p>{plant.description}</p>
-                <div className="plant-bottom-row">
-                  <p>${plant.price}</p>
-                  <p>☀️ {plant.light}</p>
-                  <p>💦 {plant.watering}x/month</p>
+      <>
+        <div className="search">
+          <form>
+            <input
+              id="name"
+              onChange={this.handleChanges}
+              value={this.state.query}
+              placeholder="Search..."
+              type="text"
+              name="textfield"
+            />
+          </form>
+        </div>
+        <main className="plant-list">
+          {this.state?.plants &&
+            this.state.plants.map((plant, index) => (
+              <div className="plant-card" key={index}>
+                <img className="plant-image" src={plant.img} alt={plant.name} />
+                <div className="plant-details">
+                  <h2 className="plant-name">{plant.name}</h2>
+                  <p className="plant-scientific-name">
+                    {plant.scientificName}
+                  </p>
+                  <p>{plant.description}</p>
+                  <div className="plant-bottom-row">
+                    <p>${plant.price}</p>
+                    <p>☀️ {plant.light}</p>
+                    <p>💦 {plant.watering}x/month</p>
+                  </div>
+                  <button
+                    className="plant-button"
+                    onClick={() => this.props.addToCart(plant)}
+                  >
+                    Add to cart
+                  </button>
                 </div>
-                <button
-                  className="plant-button"
-                  onClick={() => this.props.addToCart(plant)}
-                >
-                  Add to cart
-                </button>
               </div>
-            </div>
-          ))}
-      </main>
+            ))}
+        </main>
+      </>
     );
   }
 }
